5cc870e6eda9cb019881620394331a14d00ebf23
artyrka29 <artyrnikolaenko0709@gmail.com>
2023-09-08 20:15:44 +0300

    version2

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ window = display.set_mode((w, h))
 display.set_icon(image.load('tenis_ball.png'))
 display.set_caption("ping pong 1vs1")
 window.fill(back)
-game = True
-finish = False
-
+font.init()
+""""ШРИФТИ"""
+f1 = font.SysFont(None, 70, bold =True)
+f2 = font.SysFont(None, 50)
+""""КЛАСИ"""
 class GameSprite(sprite.Sprite):
     # конструктор класу
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -21,8 +23,8 @@ class GameSprite(sprite.Sprite):
         # кожен спрайт повинен зберігати властивість image - зображення
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
-        self.size_x = player_x
-        self.size_y = player_y
+        self.size_x = size_x
+        self.size_y = size_y
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
@@ -36,27 +38,62 @@ class Player(GameSprite):
         key_presed = key.get_pressed()
         if key_presed[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
-        key_presed = key.get_pressed()
         if key_presed[K_s] and self.rect.y < h-self.size_y:
             self.rect.y += self.speed
-            
 
+    
     def update_r(self):
-        pass
+        key_presed = key.get_pressed()
+        if key_presed[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if key_presed[K_DOWN] and self.rect.y < h-self.size_y:
+            self.rect.y += self.speed
+
 
 rokcket1 = Player('racket.png', 10, w/3, 50, 150, 5)
+rokcket2 = Player('racket.png', w - 60, h/3, 50, 150, 5)
+ball = GameSprite('tenis_ball.png', w/2, h/2, 50, 50, 7)
 
+speed_x, speed_y = 4, 4
 
+point1, point2 = 0, 0
 
-class Ball():
-    pass
-
+game = True
+finish = False
 while game:
-    time.delay(5)
     window.fill(back)
-    rokcket1.reset()
-    rokcket1.update_l()
     for e in event.get():
         if e.type == QUIT:
             game = False
+    point1_txt = f1.render(str(point2), True, (255, 255, 255))
+    point2_txt = f1.render(str(point2), True, (255, 255, 255))
+    window.blit(point1_txt, (50, 20))
+    window.blit(point2_txt, (750, 20))
+    if not finish:
+        time.delay(10)
+        rokcket1.reset()
+        rokcket1.update_l()
+        rokcket2.reset()
+        rokcket2.update_r()
+        ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if ball.rect.y <0 or ball.rect.y > h-50:
+            speed_y *= -1
+
+        if sprite.collide_rect(ball, rokcket1) or sprite.collide_rect(ball, rokcket2):
+            speed_x *= -1
+
+        if ball.rect.x > w-50:
+            point1 += 1
+            ball.rect.x = w/2
+            ball .rect.y = h/2
+
+        if ball.rect.x > w-50:
+            point2 += 1
+            ball.rect.x = w/2
+            ball .rect.y = h/2
+
+
     display.update()
